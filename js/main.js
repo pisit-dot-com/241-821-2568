bee83a68b97aeb2edc69e1fc1215d2c8b9b45f61
pisit-dot-com <pisit.has@ku.th>
2025-12-30 11:05:18 +0700

    add js

---
+++ js/main.js
@@ -0,0 +1,138 @@
+//console.log("Hello world");
+//console.log("This is javascript file");
+
+/*
+console.log("Hello world");
+console.log("This is javascript file");
+
+String,Number,Boolean,Object,Array
+
+String
+let fname = "john"; //String
+console.log("Name: ",fname);
+const PI = 3.14;
+
+Number 
+let age = 20;//Number
+let height = 5.9;//Number
+
+fname = "Alice";
+console.log("Name: ",fname);
+
+fname = "Bob";
+PI = 3.14159;
+console.log("Name: ",fname);
+console.log("Age: ",age);
+console.log("Height: ",height);
+
++ = ยวก
+- = ลบ
+* = คูณ
+/ = หาร
+% = หารเอาเศษ
+
+let number1 = 10;
+let number2 = 3;
+
+let result = number1+number2;
+console.log("ผลบวก: ",result);
+
+result = number1-number2;
+console.log("ผลลบ: ",result);
+
+result = number1*number2;
+console.log("ผลคูณ: ",result);
+
+result = number1/number2;
+console.log("ผลหาร: ",result);
+
+result = number1%number2;
+console.log("ผลหารเอาเศษ: ",result);
+
+number1 = "10";
+number2 = "3";
+
+resutl = number1+number2;
+console.log("ผลบวกString: ",number1+number2);
+
+
+== เท่ากับ
+!= ไม่เท่ากับ
+> มากกว่า
+< น้อยกว่า
+>= มากกว่าเท่ากับ
+<= น้อยกว่าเท่ากับ
+
+let number1 = 5;
+let number2 = 5;
+let condition = number1 < number2;
+console.log("condition: ",condition);
+
+//if - else condition
+if(number1 != number2){
+    console.log("this is if");
+}else if(number1 == number2){
+    console.log("this is else if"); 
+}else{
+    console.log("this is else");
+}
+
+let grade = prompt("Enter x: ");
+
+if(grade >= 80){
+    console.log("A");
+}else if(grade >= 70){
+    console.log("B");
+}else if(grade >= 60){
+    console.log("C");
+}else if(grade >= 50){
+    console.log("D");
+}else{
+    console.log("F");
+}
+
+let number1 = 5;
+let number2 = 10;
+
+let condition = (number1 > 0) || (number2 > 0);
+console.log("condition : ",condition);
+
+let age = 25;
+let gender = "femail";
+if(age >= 18 && gender == "femail"){
+    console.log("คูณสามารถเข้าร่วมกิจกรรมได้");
+}
+
+let number3 = 3;
+
+if(number3 % 2 == 0){
+    console.log("Even");
+}else{
+    console.log("Odd");
+}
+
+for(let i=1;i<=100;i++){
+    console.log("Hello from hell "+i);
+}
+
+let conter = 0;
+while(conter<=5){
+    console.log("ขอAครับอาจาร"+conter);
+    conter+=1;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
